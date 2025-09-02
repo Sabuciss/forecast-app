c7728c97e4530d1b24b835cac23858525254476f
unknown <ipb23.s.brikmane@vtdt.edu.lv>
2025-09-02 11:28:22 +0300

    vroom vroom

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+         
          $data = file_get_contents("https://emo.lv/weather-api/forecast/?city=cesis,latvia");
          $weatherData = json_decode($data, true);
          
@@ -10,14 +11,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forecast app</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-     <p> 
-        <?php echo "Pilsēta: " . $weatherData['location']['city'];?>
+<body onload="startTime()">
+    <div>
 
+    </div>
+    <p>
+    div -> parent div uh vh -> child    div vnk %
+    
+    Pilsēta: <?php echo $weatherData['city'] ['name']; ?>
+    </p>
+    <p>Current Weather</p>
+   <p>
+    Country : <?php echo $weatherData['country'] ['name']; ?>
     </p>
 
+    <div id="txt"></div>
 
+     <p type="datetime-local"></p>
+
+     <script>
+            function startTime() {
+            var today = new Date();
+            var h = today.getHours();
+            var m = today.getMinutes();
+            var s = today.getSeconds();
+            m = checkTime(m);
+            s = checkTime(s);
+            document.getElementById('txt').innerHTML =
+            h + ":" + m + ":" + s;
+            var t = setTimeout(startTime, 500);
+        }
+        function checkTime(i) {
+            if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+            return i;
+        }
+    </script>
     
 </body>
 </html>
